8d9578bf8a67271a1817abcba8db8aab349062dd
Dave86dev <davidochando1986@gmail.com>
2022-11-17 14:34:26 +0100

    Perfect debouncing on a React search

--- src/Containers/Search/Search.jsx
+++ src/Containers/Search/Search.jsx
@@ -1,10 +1,10 @@
 
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { bringMovies } from '../../Services/apiCalls';
 import './Search.css';
 
 //We are passing deestructured props for a easier use
-const Search = ({name}) => {
+const Search = ({ name }) => {
 
     //useState is the mandatory React hook method we are going to use on hooks
 
@@ -17,52 +17,73 @@ const Search = ({name}) => {
 
         //Binding the input to the hook (input criteria to hook criteria)
         setCriteria(e.target.value);
-
-        setTimeout(() => {
-            
-            if(criteria.length > 2){
-                bringMovies(criteria)
-                .then(movieResults => {
-                    console.log(movieResults)
-
-                    //Once I got the movies, I put them inside the hook movies
-                    setMovies(movieResults);
-                })
-                .catch(error => console.log(error));
-            } else if (criteria === ''){
-                    setMovies([])
-            };
-            
-        }, 500);
-
     }
 
-    useEffect(()=>{
+    useEffect(() => {
 
         //This executed when the hook criteria changes.
 
-        if(criteria != ''){
+        if (criteria !== '') {
 
-            console.log(criteria);
+            const bring = setTimeout(() => {
+
+                bringMovies(criteria)
+                    .then(movieResults => {
+                        
+                        //Here we put the movies inside the hook movies.
+                        setMovies(movieResults);
+
+                    })
+                    .catch(error => console.log(error));
+
+            }, 500);
+
+            return () => clearTimeout(bring);
+
+        } else if (criteria === ''){
+            setMovies([]);
         }
+    }, [criteria]);
 
-    },[criteria]);
+    useEffect(() => {
+        if (movies.length > 0) {
 
-    useEffect(()=>{
-        if(movies.length > 0){
-
-            console.log("helloooo",movies)
+            console.log("we got the movies...", movies)
         }
-    },[movies])
+    }, [movies]);
 
-
+    const selectFilm = (film) => {
+        console.log(film)
+    }
 
     return (
         <div className='searchDesign'>
             i am the search...of .... {name}
 
             <input name='criteria' placeholder='search a film' className='inputDesign'
-                onChange={(e)=>inputSearchHandler(e)}/>
+                onChange={(e) => inputSearchHandler(e)} />
+
+
+            {/* Conditional rendering...ONLY IF WE GOT MOVIES .... we will show 'em' */}
+
+            {
+                movies.length > 0 &&
+
+                <div className='moviesContainer'>
+                    {/* Here I will proceed to map the movies.... */}
+
+                    {
+                        movies.map(film => {
+                            return (
+                                <div className='filmDesign' key={film.id} onClick={()=>selectFilm(film)}>
+                                    {film.title}
+                                    <img className='posterDesign' src={`https://image.tmdb.org/t/p/original`+film.poster_path}/>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            }
         </div>
     )
 }
